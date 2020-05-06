6e7e66f28cfbfa3c5a9f099b93b0cf6951d185e9
root <root@192.168.2.3>
2020-05-06 15:30:06 +0530

    removed unexpected import

--- src/main/java/com/example/dao/EmployeeDAOImpl.java
+++ src/main/java/com/example/dao/EmployeeDAOImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
-import org.hibernate.HibernateException;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
