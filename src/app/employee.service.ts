d9f5726fe39fa71d41ba46213043f8160229afbd
root <root@192.168.2.3>
2020-05-06 15:20:58 +0530

    changes

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,45 +1,45 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Employees} from './Employees';
-import { Observable } from 'rxjs';
+  import { Injectable } from '@angular/core';
+  import {HttpClient} from '@angular/common/http';
+  import {Employees} from './Employees';
+  import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class EmployeeService {
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class EmployeeService {
 
-  constructor(private http : HttpClient) { }
+    constructor(private http: HttpClient) { }
 
-  employeeUrl = 'http://localhost:8888/crud_example/';
+    employeeUrl = 'http://localhost:8888/crud_example/';
 
-  save_employee = 'save_employee';
-  delete_employee ="delete_employee/";
-  get_employee_by_id ="get_employee_by_id/";
-  update_employee = "update_employee/";
-  
-  showEmployees() : Observable<any>
-  {
-    return this.http.get<any>("http://localhost:8888/crud_example/show_employees");
+    saveEmployeeUrl = 'save_employee';
+    deleteEmployeeUrl = 'delete_employee/';
+    getEmployeeByIdUrl = 'get_employee_by_id/';
+    updateEmployeeUrl = 'update_employee/';
+
+    showEmployees(): Observable<any>
+    {
+      return this.http.get<any>('http://localhost:8888/crud_example/show_employees');
+    }
+
+    getEmployeeById(id: number): Observable<Employees>
+    {
+      return this.http.get<Employees>(this.employeeUrl + this.getEmployeeByIdUrl + id);
+    }
+
+    saveEmployee(employee: any): Observable<any>
+    {
+      return this.http.post(this.employeeUrl + this.saveEmployeeUrl, employee);
+      /* return this.http.post<Employees>(this.employeeUrl, employee); */
+    }
+
+    deleteEmloyee(id: number): Observable<any>
+    {
+      return this.http.delete<any>(this.employeeUrl + this.deleteEmployeeUrl + id);
+    }
+
+    updateEmployee(emp: Employees): Observable<number>
+    {
+      return this.http.put<number>(this.employeeUrl + this.updateEmployeeUrl + emp.id, emp);
+    }
   }
-
-  getEmployeeById(id : number) : Observable<Employees>
-  {
-    return this.http.get<Employees>(this.employeeUrl+this.get_employee_by_id+id);
-  }
-
-  saveEmployee(employee : Object) : Observable<Object>
-  {
-    return this.http.post(this.employeeUrl+this.save_employee, employee);
-    /* return this.http.post<Employees>(this.employeeUrl, employee); */
-  }
-
-  deleteEmloyee(id : number) : Observable<any>
-  {
-    return this.http.delete<any>(this.employeeUrl+this.delete_employee+id);
-  }
-
-  updateEmployee(emp : Employees) : Observable<number>
-  {
-    return this.http.put<number>(this.employeeUrl+this.update_employee+emp.id, emp);
-  }
-}
--- src/app/show-employees/show-employees.component.ts
+++ src/app/show-employees/show-employees.component.ts
@@ -1,80 +1,80 @@
-import { Component, OnInit } from '@angular/core';
-import { EmployeeService } from '../employee.service';
-import { Employees } from '../Employees';
-import { Observable } from 'rxjs';
-import { FormGroup, FormBuilder } from '@angular/forms';
-import { ToastrService } from 'ngx-toastr';
+  import { Component, OnInit } from '@angular/core';
+  import { EmployeeService } from '../employee.service';
+  // import { Employees } from '../Employees';
+  // import { Observable } from 'rxjs';
+  import { FormGroup, FormBuilder } from '@angular/forms';
+  import { ToastrService } from 'ngx-toastr';
 
-@Component({
-  selector: 'app-show-employees',
-  templateUrl: './show-employees.component.html',
-  styleUrls: ['./show-employees.component.css'],
-  providers: [EmployeeService]
-})
-export class ShowEmployeesComponent implements OnInit {
+  @Component({
+    selector: 'app-show-employees',
+    templateUrl: './show-employees.component.html',
+    styleUrls: ['./show-employees.component.css'],
+    providers: [EmployeeService]
+  })
+  export class ShowEmployeesComponent implements OnInit {
 
-  allEmployees : any;
-  hideForm :boolean;
-  employeeEditForm : FormGroup;
+    allEmployees: any;
+    hideForm: boolean;
+    employeeEditForm: FormGroup;
 
-  constructor(private _empService : EmployeeService, private _fb : FormBuilder, private toastr : ToastrService) { }
 
-  ngOnInit(): void {
+    constructor(private empService: EmployeeService, private formBuilder: FormBuilder, private toastr: ToastrService) { }
 
-    this.employeeEditForm = this._fb.group({
-      id : [],
-      name : [],
-      email : []
-    });
-    
-  /* using async   
+    ngOnInit(): void {
 
-  this.allEmployees = this._empService.showEmployees();
-  console.log(this.allEmployees);
- */
-    this._empService.showEmployees().subscribe(data => {
-      this.allEmployees = data;
-      console.log(data);
-    });
+      this.employeeEditForm = this.formBuilder.group({
+        id : [],
+        name : [],
+        email : []
+      });
 
-    this.hideForm = true;
-  }
+    /* using async
 
-  editEmployee(id : number)
-  {
-    this.hideForm = false;
-
-    this._empService.getEmployeeById(id).subscribe(empl => {
-      this.employeeEditForm.controls['id'].setValue(empl.id);
-      this.employeeEditForm.controls['name'].setValue(empl.name);
-      this.employeeEditForm.controls['email'].setValue(empl.email);
-    });
-  }
-
-  deleteEmployee(id :number)
-  {
-    console.log("Delete ID :" +id);
-   this._empService.deleteEmloyee(id).subscribe(data => {
-    this._empService.showEmployees().subscribe(data => {
-      this.allEmployees = data;
-      this.toastr.info("Employee Deleted Succesfully");
-      console.log(data);
-    });
-   });
-  }
-
-  updateEmployee()
-  {
-    var employeeFormValues = this.employeeEditForm.value;
-
-     this._empService.updateEmployee(employeeFormValues).subscribe(data => {
-      this._empService.showEmployees().subscribe(data => {
+    this.allEmployees = this.empService.showEmployees();
+    console.log(this.allEmployees);
+  */
+      this.empService.showEmployees().subscribe(data => {
         this.allEmployees = data;
         console.log(data);
-        this.hideForm = true;
-        this.toastr.success("Employee Updated Succesfully");
+      });
+
+      this.hideForm = true;
+    }
+
+    editEmployee(id: number)
+    {
+      this.hideForm = false;
+
+      this.empService.getEmployeeById(id).subscribe(empl => {
+        this.employeeEditForm.controls.id.setValue(empl.id);
+        this.employeeEditForm.controls.name.setValue(empl.name);
+        this.employeeEditForm.controls.email.setValue(empl.email);
+      });
+    }
+
+    deleteEmployee(id: number)
+    {
+        console.log('Delete ID :' + id);
+        this.empService.deleteEmloyee(id).subscribe(() => {
+        this.empService.showEmployees().subscribe(data => {
+        this.allEmployees = data;
+        this.toastr.info('Employee Deleted Succesfully');
+        console.log(data);
       });
     });
+    }
 
+    updateEmployee()
+    {
+      const employeeFormValues = this.employeeEditForm.value;
+
+      this.empService.updateEmployee(employeeFormValues).subscribe(() => {
+        this.empService.showEmployees().subscribe(data => {
+          this.allEmployees = data;
+          console.log(data);
+          this.hideForm = true;
+          this.toastr.success('Employee Updated Succesfully');
+        });
+      });
+    }
   }
-}
