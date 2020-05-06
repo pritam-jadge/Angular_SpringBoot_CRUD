a15863621e659d8bedb31d1292937a27f4916411
root <root@192.168.2.3>
2020-05-06 16:07:27 +0530

    removed unwanted imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
-import { Component } from '@angular/core';
-import { EmployeeService } from './employee.service';
-import { Observable } from 'rxjs';
-import { Employees } from './Employees';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'employeeCRUD';
-}
+  import { Component } from '@angular/core';
+  @Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+  })
+  export class AppComponent {
+    title = 'employeeCRUD';
+  }
