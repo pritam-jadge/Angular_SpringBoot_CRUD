286d74d95b9c83d0db81e27d601e6205c8b8a2e0
root <root@192.168.2.3>
2020-05-06 15:03:32 +0530

    removed white spaces

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { from } from 'rxjs';
 import { ShowEmployeesComponent } from './show-employees/show-employees.component';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, enableProdMode } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
-import {ReactiveFormsModule,FormsModule} from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigatorComponent } from './navigator/navigator.component';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
-import {EmployeeDB} from './testData';
+/* import {EmployeeDB} from './testData';
 
-import { from } from 'rxjs';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+ */
 import { EmployeeService } from './employee.service';
 import { ShowEmployeesComponent } from './show-employees/show-employees.component';
 import { ToastrModule } from 'ngx-toastr';
@@ -34,7 +34,7 @@ import { ToastrModule } from 'ngx-toastr';
     ToastrModule.forRoot()
   ],
   providers: [EmployeeService],
-  
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
